b55313789e81782e4fbd1a2f2de89fe041108b06
ndanni <43090769+ndanni@users.noreply.github.com>
2018-10-30 11:01:18 +0100

    Add files via upload

---
+++ quiz4.php
@@ -0,0 +1,15 @@
+<?php
+for($x =100; $x <=150; $x++){
+    echo "The number is: $x <br>";
+}
+?>
+
+<?php
+
+for($x = 0; $x <= 50; $x++) {
+    if ($x % 2 == 0){
+    echo "The number is: $x <br>";
+    echo"";
+    }
+}
+?>
